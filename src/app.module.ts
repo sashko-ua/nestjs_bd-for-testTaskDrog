3b2cb52c6afd7ceecaa8da35a1e07884742d8ce4
Sashko <sashkoace@gmail.com>
2020-10-28 16:40:27 +0200

    start from begin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { config } from './orm.config'
-import { PhotoController } from './photo/photo.controller';
-import { PhotoService } from './photo/photo.service';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
-  controllers: [AppController, PhotoController],
-  providers: [AppService, PhotoService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/photo/photo.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Url } from "url";
-
-export interface Photo {
-    readonly id: number;
-    readonly url: Url;
-}
